283d2c9747217e28d22ccdd6e167a4184fe919fd
chalith <desaman.chalith@gmail.com>
2022-05-19 12:17:28 +0530

    Added test functions to delete hosts

--- hook-emulator/test/state-operations.js
+++ hook-emulator/test/state-operations.js
@@ -1,4 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
+const codec = require('ripple-address-codec');
 
 const DB_FILE = 'hook-emulator.sqlite';
 
@@ -38,6 +39,20 @@ const setStateValue = async (db, hexKey, hexValue) => {
     });
 }
 
+const deleteStateValue = async (db, hexKey) => {
+    const key = Buffer.from(hexKey, 'hex');
+    const query = `DELETE FROM state WHERE key = ?;`;
+    return new Promise((resolve, reject) => {
+        db.run(query, [key], function (err) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({ changes: this.changes });
+        });
+    });
+}
+
 const executeFunc = async (dbFile, callback) => {
     const db = new sqlite3.Database(dbFile);
     await callback(db);
@@ -54,8 +69,43 @@ const updateHostCount = async (db) => {
     await setStateValue(db, hexKey, buf.toString('hex'));
 }
 
+const deletHosts = async (db) => {
+    const addresses = [
+        'rpCqZSTN9YkwV5knsbujGRE1Q37mRTwH7T',
+        'rs9YfGnrgkqLXsxh1MUPfkvSiwkYEY6yhw',
+        'rs65gGVF1cJCRrCphpYBjTRWgHab91AHkw',
+        'rnynbxsbC1pBcYTUumQq6zyyejVga7KuHD',
+        'rnynbxsbC1pBcYTUumQq6zyyejVga7KuHD',
+        'rHresLrv5cn2HtbJkfvPQa1HDrYZxBFXuw',
+        'rJcPq2LrTxMgbtXta9zgaHbjt2zMyGT56x',
+        'rKKGyagwAW2UFoZ5voeQztoqWmm6zJorbY',
+        'rMD7bxfBMbmaCaVmA5Qf5YnpUmCnCjmue5',
+        'rPSDgdEAsL3LRmxthqj3TECuR4rpSToogz'
+    ];
+    const countKey = '4556523200000000000000000000000000000000000000000000000000000000';
+    let delCount = 0;
+    for (const address of addresses) {
+        const accKey = Buffer.from('4556520300000000000000000000000000000000000000000000000000000000', 'hex');
+        codec.decodeAccountID(address).copy(accKey, accKey.length - 20);
+        const addrVal = await getStateValue(db, accKey.toString('hex'));
+        if (addrVal.length) {
+            const idKey = Buffer.from('4556520200000000000000000000000000000000000000000000000000000000', 'hex');
+            addrVal[0].value.copy(idKey, 4, 4);
+            await deleteStateValue(db, idKey.toString('hex'));
+            delCount++;
+        }
+        await deleteStateValue(db, accKey.toString('hex'));
+    }
+    const countVal = await getStateValue(db, countKey);
+    const count = countVal[0].value.readUInt32BE();
+    const buf = Buffer.allocUnsafe(4);
+    buf.writeUInt32BE(count - delCount);
+    await setStateValue(db, countKey, buf.toString('hex'));
+}
+
 const main = async () => {
-    await executeFunc(DB_FILE, updateHostCount);
+    // await executeFunc(DB_FILE, updateHostCount);
+    // await executeFunc(DB_FILE, deletHosts);    
 }
 
 main().catch(console.error);
