7a853745ab1d10e0109f91f1814e32f89e79a7b6
chalith <desaman.chalith@gmail.com>
2022-07-29 10:48:00 +0530

    Changed the moment size to 1190

--- src/constants.h
+++ src/constants.h
@@ -39,7 +39,7 @@
 
 // Default values.
 const uint8_t HOOK_INITIALIZER_ADDR[35] = "rMv668j9M6x2ww4HNEF4AhB8ju77oSxFJD";
-const uint16_t DEF_MOMENT_SIZE = 900;
+const uint16_t DEF_MOMENT_SIZE = 1190;
 const uint64_t DEF_MINT_LIMIT = 72253440;
 const uint64_t DEF_HOST_REG_FEE = 5120;
 const uint64_t DEF_FIXED_REG_FEE = 5;
