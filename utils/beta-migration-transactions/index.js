aa259b9428e8043797a03873bec43ffc8c01e72e
Udith-Gayan <udith.indrakantha@gmail.com>
2022-08-02 13:24:32 +0530

    Missing parameters added.

--- utils/beta-migration-transactions/index.js
+++ utils/beta-migration-transactions/index.js
@@ -1,9 +1,12 @@
 
 const process = require('process');
 const fs = require('fs');
+const codec = require('ripple-address-codec');
 const { XrplAccount, XrplApi, EvernodeConstants, RegistryClient } = require('evernode-js-client');
 
 const MIN_XRP = "1";
+const INIT_MEMO_TYPE = "evnInitialize";
+const INIT_MEMO_FORMAT = "hex";
 const CONFIG_FILE = './evr-transfer-config.json';
 
 // Hooks V2 TEST NET URL.
@@ -33,6 +36,11 @@ async function transferEvrToRegistry(config) {
 
     const regClient = new RegistryClient(config.registry.address);
     await regClient.connect();
+
+    // Get issuer and foundation cold wallet account ids.
+    let memoData = Buffer.allocUnsafe(40);
+    codec.decodeAccountID(regClient.configs.issuerAddress).copy(memoData);
+    codec.decodeAccountID(config.purchaser.address).copy(memoData, 20);
 
     const purchaserWallet = new XrplAccount(config.purchaser.address, config.purchaser.secret, { xrplApi: xrplApi });
     res = await purchaserWallet.makePayment(config.registry.address, '51609600', EvernodeConstants.EVR, regClient.configs.issuerAddress);
