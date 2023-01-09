334dd3a3a777d5508bb39b359084daacfdf7bc4b
Udith-Gayan <udith.indrakantha@gmail.com>
2023-01-09 10:20:56 +0530

    A validity check added

--- setgrant.js
+++ setgrant.js
@@ -43,21 +43,26 @@ else {
     let hook2Hashes = await getHookHashes(registryAddress);
     let hook3Hashes = await getHookHashes(heartbeatAddress);
 
-    const hookTx = {
-        Account: account.classicAddress,
-        TransactionType: "SetHook",
-        Hooks: hookHashes.map(() => {
-            return {
-                Hook: {
-                    HookGrants: [{
-                        ...hook2Hashes.map(h => ({ HookGrant: { Authorize: registryAddress, HookHash: h } })),
-                        ...hook3Hashes.map(h => ({ HookGrant: { Authorize: heartbeatAddress, HookHash: h } }))
-                    }]
-                }
-            };
-        })
-    };
+    if(hookHashes && hookHashes.length && hook2Hashes && hook2Hashes.length && hook3Hashes && hook3Hashes.length) {
+        const hookTx = {
+            Account: account.classicAddress,
+            TransactionType: "SetHook",
+            Hooks: hookHashes.map(() => {
+                return {
+                    Hook: {
+                        HookGrants: [{
+                            ...hook2Hashes.map(h => ({ HookGrant: { Authorize: registryAddress, HookHash: h } })),
+                            ...hook3Hashes.map(h => ({ HookGrant: { Authorize: heartbeatAddress, HookHash: h } }))
+                        }]
+                    }
+                };
+            })
+        };
+    
+        submitTxn(governorSecret, hookTx).then(res => { console.log(res); }).catch(console.error).finally(() => process.exit(0));
+    } else {
+        throw("Error in Hook hashes.");
+    }
 
-    submitTxn(governorSecret, hookTx).then(res => { console.log(res); }).catch(console.error).finally(() => process.exit(0));
 
 }
