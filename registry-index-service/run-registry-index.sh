4336965c0e73026c81188fb9f2fb1290186700ef
Chalith Desaman <desaman.chalith@gmail.com>
2023-05-17 15:36:40 +0530

    Enable ability to configure dev index (#182)

--- registry-index-service/run-registry-index.sh
+++ registry-index-service/run-registry-index.sh
@@ -8,6 +8,8 @@ hook2_setup="hook2-setup"
 hook3_setup="hook3-setup"
 index="index.js"
 data_dir=$(pwd)
+env=$([ -f .env ] && cat .env)
+[ -z $env ] && env="dev"
 
 # If index does not exist in current directory, this is a dev execution.
 # Setup the dev repo paths.
@@ -58,6 +60,7 @@ function create_service() {
                 WorkingDirectory=$(pwd)
                 Environment=\"DATA_DIR=$data_dir\"
                 Environment=\"ACTION=recover\"
+                Environment=\"ENV=$env\"
                 ExecStart=$(which node) $index $arg1
                 Restart=on-failure
                 RestartSec=5
@@ -125,6 +128,6 @@ elif [ ! -z "$arg2" ]; then # If 2nd param is given.
     exit 0
 fi
 
-DATA_DIR=$data_dir $(which node) $index $arg1
+ENV=$env DATA_DIR=$data_dir $(which node) $index $arg1
 
 exit 0
