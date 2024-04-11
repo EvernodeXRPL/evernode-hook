92a044f605337b35f71bad9a7e0505646277b780
kithminisg <kithminishanika@gmail.com>
2024-04-11 09:37:17 +0530

    Keylet based hook hash retrieval

--- evernode-governor-hook/src/governor.h
+++ evernode-governor-hook/src/governor.h
@@ -75,7 +75,8 @@
 #define COPY_CANDIDATE_HASHES(lhsbuf, rhsbuf)   \
     COPY_32BYTES(lhsbuf, rhsbuf);               \
     COPY_32BYTES((lhsbuf + 32), (rhsbuf + 32)); \
-    COPY_32BYTES((lhsbuf + 64), (rhsbuf + 64));
+    COPY_32BYTES((lhsbuf + 64), (rhsbuf + 64)); \
+    COPY_32BYTES((lhsbuf + 96), (rhsbuf + 96));
 
 // Default values.
 const uint8_t HOOK_INITIALIZER_ADDR[35] = "rfQhDoCjcVzp14MLhSryTTsyU2VESZ5nAt";
@@ -113,20 +114,30 @@ const uint8_t NEW_MOMENT_TYPE = TIMESTAMP_MOMENT_TYPE;
      IS_BUFFER_EMPTY_2((buf + 8)) &&      \
      IS_BUFFER_EMPTY_1((buf + 10)))
 
-#define IS_HOOKS_VALID(hook_keylets_ptr, hooks_exists)              \
-    {                                                               \
-        hooks_exists = 0;                                           \
-        int64_t hook_slot = slot_set(hook_keylets_ptr, 34, 0);      \
-        if (hook_slot >= 0)                                         \
-        {                                                           \
-            hook_slot = slot_set(hook_keylets_ptr + 34, 34, 0);     \
-            if (hook_slot >= 0)                                     \
-            {                                                       \
-                hook_slot = slot_set(hook_keylets_ptr + 68, 34, 0); \
-                if (hook_slot >= 0)                                 \
-                    hooks_exists = 1;                               \
-            }                                                       \
-        }                                                           \
+#define IS_HOOKS_VALID(hook_keylets_ptr, hook_hashes, hooks_exists)      \
+    {                                                                    \
+        hooks_exists = 0;                                                \
+        int64_t hook_slot = slot_set(hook_keylets_ptr, 34, 0);           \
+        if (hook_slot >= 0)                                              \
+        {                                                                \
+            slot(&hook_hashes[0], 32, hook_slot);                        \
+            hook_slot = slot_set(hook_keylets_ptr + 34, 34, 0);          \
+            if (hook_slot >= 0)                                          \
+            {                                                            \
+                slot(&hook_hashes[32], 64, hook_slot);                   \
+                hook_slot = slot_set(hook_keylets_ptr + 68, 34, 0);      \
+                if (hook_slot >= 0)                                      \
+                {                                                        \
+                    slot(&hook_hashes[64], 92, hook_slot);               \
+                    hook_slot = slot_set(hook_keylets_ptr + 102, 34, 0); \
+                    if (hook_slot >= 0)                                  \
+                    {                                                    \
+                        slot(&hook_hashes[64], 92, hook_slot);           \
+                        hooks_exists = 1;                                \
+                    }                                                    \
+                }                                                        \
+            }                                                            \
+        }                                                                \
     }
 
 #define CHECK_RUNNING_HOOK(account, hook_hash, is_valid)                    \
