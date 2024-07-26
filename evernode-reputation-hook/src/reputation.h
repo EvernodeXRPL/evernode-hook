92e8a651f5f897f15871c718da393ab9f0806de1
Chalith Desaman <desaman.chalith@gmail.com>
2024-07-26 16:52:14 +0530

    Consider cluster size for score evaluation (#228)

--- evernode-reputation-hook/src/reputation.h
+++ evernode-reputation-hook/src/reputation.h
@@ -8,7 +8,11 @@
 #define FOREIGN_REF SBUF(NAMESPACE), state_hook_accid, ACCOUNT_ID_SIZE
 
 // Minimum requirement of denominator to update score.
-#define MIN_DENOM_REQUIREMENT(universe_size) \
-    universe_size * 80 / 100
+#define MIN_DENOM_REQUIREMENT(cluster_size) \
+    cluster_size * 80 / 100
+
+// Minimum requirement of cluster size to update score.
+#define MIN_CLUSTER_REQUIREMENT(universe_size) \
+    universe_size * 50 / 100
 
 #endif
